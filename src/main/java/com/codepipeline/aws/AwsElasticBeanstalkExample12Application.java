8b46f5ad043adc81f3a2609450ccd1129801458f
maknikarrushikesh <maknikarrushikesh2010@gmail.com>
2020-01-05 14:58:51 +0530

    My First commit towards Pipeline

---
+++ src/main/java/com/codepipeline/aws/AwsElasticBeanstalkExample12Application.java
@@ -0,0 +1,13 @@
+package com.codepipeline.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticBeanstalkExample12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticBeanstalkExample12Application.class, args);
+	}
+
+}
